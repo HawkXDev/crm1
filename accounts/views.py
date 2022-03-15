a5f1aba35d9d9233caa304126df909ab9bef7857
sokolserg <sergsokolkin@gmail.com>
2022-03-15 14:08:08 +0300

    rendering data to templates, template tags

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from .models import Product, Customer, Order
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    pending = orders.filter(status='Pending').count()
+    delivered = orders.filter(status='Delivered').count()
+
+    context = {'customers': customers, 'orders': orders, 'total_customers': total_customers,
+               'total_orders': total_orders, 'pending': pending, 'delivered': delivered}
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 
 def customer(request):
